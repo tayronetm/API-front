675f4f0542791cdc8bccd359b48ffd730f09be75
tayronetm <tayronetnm@gmail.com>
2018-03-07 15:37:51 -0300

    usando pipes

--- src/app/campo-colorido.directive.ts
+++ src/app/campo-colorido.directive.ts
@@ -1,18 +1,22 @@
-import { Directive, HostListener, HostBinding } from '@angular/core';
+import { Directive, HostListener, HostBinding, Input } from '@angular/core';
 
 @Directive({
   selector: '[appCampoColorido]'
 })
 export class CampoColoridoDirective {
 
+  @Input() cor = '';
+
   @HostBinding('style.backgroundColor') colorBackground : string;
 
   @HostListener('focus') onFocus(){
-    this.colorBackground = 'yellow';
+    this.colorBackground = this.cor;
   }
 
   @HostListener('blur') leaveFocus(){
     this.colorBackground= 'transparent';
   }
+
+  
 
 }

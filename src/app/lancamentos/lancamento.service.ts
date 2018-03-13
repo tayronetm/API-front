5a810297da6d520ff5582d2bbee7618744e9860c
tayronetm <tayronetnm@gmail.com>
2018-03-13 18:16:34 -0300

    fitro por data

--- src/app/lancamentos/lancamento.service.ts
+++ src/app/lancamentos/lancamento.service.ts
@@ -1,10 +1,13 @@
-import { Http, Headers } from '@angular/http';
+import { Http, Headers, URLSearchParams } from '@angular/http';
 import { Injectable } from '@angular/core';
 
 import 'rxjs/add/operator/toPromise';
+import * as moment from 'moment';
 
-export interface LancamentoFiltro{
+export interface LancamentoFiltro {
   descricao: string;
+  dataVencimentoInicio: Date;
+  dataVencimentoFim: Date;
 }
 
 @Injectable()
@@ -14,17 +17,28 @@ export class LancamentoService {
 
   constructor(private http: Http) { }
 
-  pesquisar(filtro: any): Promise<any> {
+  pesquisar(filtro: LancamentoFiltro): Promise<any> {
     const params = new URLSearchParams();
     const headers = new Headers();
+
     headers.append('Authorization', 'Basic YWRtaW5AYWxnYW1vbmV5LmNvbTphZG1pbg==');
 
-    if (filtro.descricao){
+    if (filtro.descricao) {
       params.set('descricao', filtro.descricao);
     }
 
+    if (filtro.dataVencimentoInicio) {
+      params.set('dataVencimentoDe',
+        moment(filtro.dataVencimentoInicio).format('YYYY-MM-DD'));
+    }
+
+    if (filtro.dataVencimentoFim) {
+      params.set('dataVencimentoAte',
+        moment(filtro.dataVencimentoFim).format('YYYY-MM-DD'));
+    }
+
     return this.http.get(`${this.lancamentosUrl}?resumo`,
-     { headers, search: filtro })
+        { headers, search: params })
       .toPromise()
       .then(response => response.json().content)
   }
